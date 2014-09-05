a5bbece1c6d28f2cce140412e34ff4379ea9bd58
orange <orange@jambonb.org>
2014-09-05 12:20:58 +0800

    default config

---
+++ web/config/profiles/preprod.js
@@ -0,0 +1,21 @@
+{
+    "lte":{
+        "title":"Preprod",
+        "skin":"skin-black",
+        "org":""
+    },
+	"pdo":{
+        "host":     "",
+        "name":     "",
+        "driver":   "",
+        "pass":     "",
+        "port":     "",
+        "user":     ""
+	},
+    "mongo":{
+        "host": "",
+        "user": "",
+        "pass": "",
+        "port": "27017"
+    }
+}
