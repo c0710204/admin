27c533c2c14225189819ee478e4dcfe4b3c55b05
orange <orange@jambonb.org>
2014-09-18 17:41:32 +0800

    New screens

--- web/course_enrollments/download.php
+++ web/course_enrollments/download.php
@@ -8,6 +8,7 @@ require __DIR__."/../../vendor/autoload.php";
 use Admin\AdminLte;
 use Admin\EdxApp;
 use Admin\EdxCourse;
+use Admin\XLSXWriter;
 
 $admin = new AdminLte();
 $admin->ctrl();
@@ -23,10 +24,48 @@ if (!$edxCourse->exist($course_id)) {
     die("Course not found");
 }
 
-$data=$edxApp->enrollments($course_id, 1);
 
+$header = array(
+    'user_id'=>'string',
+    'email'=>'string',
+    'first_name'=>'string',
+    'last_name'=>'string',
+    'active'=>'string',
+    //'email'=>'string',
+    //'month'=>'string',
+    //'amount'=>'money',
+    //'first_event'=>'datetime',
+    'enrolled'=>'date',
+);
+
+
+$data=$edxApp->enrollments($course_id);
+//echo "<pre>";
+$data1=[];
 foreach ($data as $r) {
-    print_r($r);
+    $row=[];
+    //print_r($r);
+    $user=$edxApp->user($r['user_id']);
+    //print_r($user);
+    $data1[]=[$r['user_id'],$user['email'],$user['first_name'], $user['last_name'], $r['is_active'], $r['created']];
 }
 
-die("ok");
+//print_r($data1);
+//user_id, email, first_name, last_name, joined
+
+$filename = "enrollments.xlsx";
+
+header('Content-disposition: attachment; filename="'.XLSXWriter::sanitize_filename($filename).'"');
+header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+header('Content-Transfer-Encoding: binary');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+
+$writer = new XLSXWriter();
+$writer->setAuthor('FBM');
+$writer->writeSheet($data1, "$course_id", $header);
+$writer->writeSheet([], 'Course info');
+$writer->writeToFile($filename);
+$writer->writeToStdOut();
+
+//die("ok");
--- web/course_enrollments/index.php
+++ web/course_enrollments/index.php
@@ -13,31 +13,23 @@ $admin = new AdminLte();
 $admin->title("Course enrollments");
 echo $admin->printPrivate();
 
-
-
 if (isset($_GET['course_id'])) {
     $course_id=@$_GET['course_id'];
-} elseif(isset($_GET['id'])) {
+} elseif (isset($_GET['id'])) {
     $course_id=@$_GET['id'];
 } else {
     die("Error");
 }
 
-
 $edxapp = new Admin\EdxApp();
 $edxCourse = new Admin\EdxCourse($course_id);
 
-// metadata
-
-//$meta = $edxCourse->metadata($course_id);
-
-// http://edx.readthedocs.org/projects/devdata/en/latest/course_data_formats/course_xml.html
-// http://edx.readthedocs.org/projects/devdata/en/latest/course_data_formats/grading.html
 echo "<input type='hidden' id='course_id' value='$course_id'>";
 ?>
 
 <section class="content-header">
-    <h1><a href='../courses/'><i class='fa fa-book'></i></a> Enrollments in course : <?php echo $edxapp->courseName($course_id)?></h1>
+    <h1><i class='fa fa-user'></i> <i class='fa fa-angle-right'></i> <i class='fa fa-book'></i> 
+    Enrollments in course : <a href='../course/?id=<?php echo $course_id?>'><?php echo $edxapp->courseName($course_id)?></a></h1>
     <ol class="breadcrumb">
         <li class="active"><i class="fa fa-book"></i> <?php echo explode('/', $course_id)[0]?></li>
         <li class="active"><?php echo explode('/', $course_id)[1]?></li>
@@ -51,22 +43,112 @@ echo "<input type='hidden' id='course_id' value='$course_id'>";
 <?php
 if (!$edxCourse->exist($course_id)) {
     echo new Admin\Callout('Danger', 'Course "'.$course_id.'" not found', 'The course was not found or not available');
-    //exit;
 }
 ?>
+
+
 
 <!-- Main row -->
 <div class="row">
     <!-- Left col -->
     <section class="col-lg-6 connectedSortable">
         <?php
+
+        include "box_enroll_info.php";
+
+        include "box_search.php";
         ?>
     </section>
 
     <!-- Right col -->
     <section class="col-lg-6 connectedSortable">
         <?php
-        //include "course_overview.php";
+        $box=new Admin\SolidBox;
+        $box->id("boxenroll");
+        $box->title("List users");
+        $box->icon("fa fa-list");
+        $box->loading(true);
+        echo $box->html("Search");
         ?>
     </section>
 </div>
+
+
+
+<script>
+
+
+function getEnrollData(){
+    
+    //console.log('getEnrollData()');
+    
+    $("#boxenroll .overlay, #boxenroll .loading-img").show();//loading
+    
+    var r=$.ajax({
+        type: "POST",
+        url: "ctrl.php",
+        dataType: "json",
+        data: {
+            'do':'listEnroll',
+            'course_id':$('#course_id').val(),
+            'search':$('#searchStr').val(),
+            'limit':$('#limit').val()
+        }
+    });
+
+    r.done(function(json){
+        
+        //console.log(json);
+        //alert(json);
+        
+        /*
+        $.each(json,function(k,v){
+            //json[k].start=new Date(v.start);
+            //json[k].end=new Date(v.end);
+        });
+        */
+        
+        dispEnroll(json, $("#boxenroll .box-body"));
+        $("#boxenroll .overlay, #boxenroll .loading-img").hide();
+    });
+
+    r.fail(function(a,b,c){
+        console.log('fail',a,b,c);
+        $("#boxenroll .box-body").html(a.responseText);
+    });
+}
+
+function dispEnroll(json, target){
+    //console.log('dispEnroll()');
+    
+    var h=[];
+    h.push("<table class='table table-striped table-condensed'>");
+    h.push("<thead>");
+    h.push("<th>User</th>");
+    h.push("<th>Joined</th>");
+    h.push("</thead>");
+    h.push("<tbody>");
+    $.each(json,function(k,v){
+        h.push('<tr>');
+        h.push('<td><a href=../user/?id='+v.user_id+'>'+v.username);
+        h.push('<td>'+v.created);
+        h.push('</tr>');
+    });
+    h.push("</tbody>");
+    h.push("</table>");
+
+    target.html(h.join(''));
+}
+
+
+$(function(){
+    //console.log('ready');
+
+    //$("#boxenroll .overlay, #boxenroll .loading-img").hide();
+    $('#searchStr, #limit').change(function(){
+        getEnrollData();
+    });
+    getEnrollData();
+});
+
+</script>
