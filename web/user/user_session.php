82ea3c81e69cbebae91a56983029a32eab0508d0
jambonbill <jambonbill@gmail.com>
2015-01-05 22:42:51 +0800

    New display

--- web/user/user_session.php
+++ web/user/user_session.php
@@ -4,38 +4,37 @@
 // var_dump($sessions);exit;
 
 $body=[];
-//$body[]="<pre>".print_r($sessions,true)."</pre>";
-$body[]="<table class='table table-condensed'>";
-$body[]="<thead>";
-//$body[]="<th>Sess";
-$body[]="<th>Session start";
-$body[]="<th>Session start";
-$body[]="<th>Length";
-$body[]="</thead>";
 
-//echo "<pre>".print_r($sessions,true)."</pre>";
+if (count($user_sessions)) {
 
-foreach ($user_sessions as $session) {
-    
-    //var_dump($v);
-    //$time=$edxApp->session_time($session);
-    //print_r($session);exit;
-    //continue;
-    $session_id=$session['session'];
-    
-    $body[]="<tr>";
-    $body[]="<td>";
-    $body[]="<a href='../session/?id=$session_id'>";
-
-    $body[]=date("D d M Y ",strtotime($session['date_from'])) . "</a>";
-    $body[]="<td>".substr($session['date_from'],0,16);
-    //$body[]="<td>".$session['date_to'];
-    $length=strtotime($session['date_to']) - strtotime($session['date_from']);
-    $minutes=round($length/60);
-    $body[]="<td>".$minutes." min";
+    $body[]="<table class='table table-condensed'>";
+    $body[]="<thead>";
+    $body[]="<th>Session start";
+    $body[]="<th>Session start";
+    $body[]="<th>Length";
+    $body[]="</thead>";
+    //echo "<pre>".print_r($sessions,true)."</pre>";
+    $body[]="<tbody>";
+    foreach ($user_sessions as $session) {
+        
+        $session_id=$session['session'];
+        
+        $body[]="<tr>";
+        $body[]="<td>";
+        $body[]="<a href='../session/?id=$session_id'>";
+        $body[]=date("D d M Y ",strtotime($session['date_from'])) . "</a>";
+        $body[]="<td>".substr($session['date_from'],0,16);
+        $length=strtotime($session['date_to']) - strtotime($session['date_from']);
+        $minutes=round($length/60);
+        $body[]="<td>".$minutes." min";
+    }
+    $body[]="</tbody>";
+    $body[]="</table>";
+} else {
+    $body[]="<i class='fa fa-warning' style='color:c00'></i> No session data";
 }
-//$body[]="</table>";
-$body[]="</table>";
+
+
 
 
 
@@ -45,7 +44,7 @@ $box->icon("fa fa-bolt");
 $box->type("primary");
 $box->title("Sessions");
 //$box->loading(true);
-$foot="<a href='../user_calendar/?user_id=$USERID' class='btn btn-default'><i class='fa fa-calendar'></i> Calendar</a>";
+$foot="<a href='../user_calendar/?user_id=$USERID' class='btn btn-default'><i class='fa fa-calendar'></i> User calendar</a>";
 echo $box->html($body,$foot);
 
 
