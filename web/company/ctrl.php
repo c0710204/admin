1ba761201a2fb5146877a5b6f420413dff7b7421
jambonbill <jambonbill@gmail.com>
2015-01-21 11:50:10 +0800

    new delete company function

--- web/company/ctrl.php
+++ web/company/ctrl.php
@@ -12,6 +12,17 @@ $edxCrm= new Admin\EdxCrm();
 
 switch($_POST['do']){
 
+	case 'delete':
+		//print_r($_POST);
+		$company_id=$_POST['id']*1;
+		if($company_id>0){
+			$sql="DELETE FROM edxcrm.companies WHERE c_id=$company_id LIMIT 1;";
+			$admin->db()->query($sql) or die("Error : $sql");
+			//echo $sql;
+			die('document.location.href="../companies/";');
+		}
+		exit;
+		break;
 
     default:
         echo "ctrl error : unknow action";
